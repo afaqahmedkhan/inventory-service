72eed095c911a486eef09ff8d64dd3f7bfd654ca
Afaq Ahmed Khan <afaq.qwe@gmail.com>
2024-08-10 11:45:09 +0200

    wrapping up inventory service added inventory model added inventory service added inventory controller and respository added method isInStock in controller and service and repository

---
+++ inventory-service/src/main/java/com/afaq/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.afaq.microservices.inventory.repository;
+
+import com.afaq.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+
+}
